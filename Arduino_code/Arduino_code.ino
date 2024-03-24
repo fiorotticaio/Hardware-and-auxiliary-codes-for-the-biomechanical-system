097faf39eb57c5fb965377a0a2d6895b8cb0a485
Caio Alves Fiorotti <caiofiorotti@gmail.com>
2024-03-24 11:36:40 -0300

    :art: switching to English and formatting

--- Arduino_code/Arduino_code.ino
+++ Arduino_code/Arduino_code.ino
@@ -1,40 +1,35 @@
 /*
-Filtro passa-baixa implementado como uma média ponderada entre o valor atual do sensor 
-e o valor filtrado anteriormente, e não como um filtro analógico tradicional com uma frequência de corte definida.
-
-O fator alpha no código serve como um parâmetro de suavização, e não como uma frequência de corte tradicional.
- Quanto menor o valor de alpha, mais suave será a resposta do filtro, e quanto maior o valor de alpha, mais rápida será a resposta do filtro.
+Low pass filter implemented as a weighted average between the current sensor value
+and the previously filtered value, rather than like a traditional analog filter with a defined cutoff frequency.
+The alpha factor in the code serves as a smoothing parameter, not a traditional cutoff frequency.
+The lower the alpha value, the smoother the filter response, and the higher the alpha value, the faster the filter response.
 */
 
-// Parâmetros do filtro passa-baixa
-const float alpha = 0.1; // Fator de suavização (0 < alpha < 1)
-float filtered_sig_flex = 0; // Valor filtrado inicial
+/* Low Pass Filter Parameters */
+const float alpha = 0.1; // Smoothing factor (0 < alpha < 1)
+float filtered_sig_flex = 0; // Initial filtered flexion value 
+float filtered_sig_ext = 0; // Initial filtered extension value
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  float sig_flex = analogRead(A0); // Read the signal
-  // float sig_ext = analogRead(A1);
-  filtered_sig_flex = alpha * sig_flex + (1 - alpha) * filtered_sig_flex; // Aplica o filtro passa-baixa
-  // filtered_sig_ext = alpha * sig_ext + (1 - alpha) * filtered_sig_ext; // Aplica o filtro passa-baixa
+  float sig_flex = analogRead(A0); // Read the flexion sensor value
+  float sig_ext = analogRead(A1); // Read the extension sensor value
+
+  filtered_sig_flex = alpha * sig_flex + (1 - alpha) * filtered_sig_flex; // Apply the low pass filter
+  filtered_sig_ext = alpha * sig_ext + (1 - alpha) * filtered_sig_ext; // Apply the low pass filter
 
   float sig_flex_millivolts = (filtered_sig_flex/1023) * 5; // Convert to millivolts
-  float sig_flex_volts = sig_flex_millivolts * 1000;
+  float sig_flex_volts = sig_flex_millivolts * 1000; // Convert to volts
 
-  // float sig_ext_millivolts = (sig_ext/1023) * 5; // Convert to millivolts
-  // float sig_ext_volts = sig_ext_millivolts * 1000;
+  float sig_ext_millivolts = (sig_ext/1023) * 5; // Convert to millivolts
+  float sig_ext_volts = sig_ext_millivolts * 1000; // // Convert to millivolts
 
-
-  // Serial.print(sig_flex_volts);
-  // Serial.print(",");
-  // Serial.println(sig_ext_volts);
-
-  Serial.println(sig_flex_volts);
-
+  Serial.print(sig_flex_volts);
+  Serial.print(",");
+  Serial.println(sig_ext_volts);
 
   delay(10);
 }
