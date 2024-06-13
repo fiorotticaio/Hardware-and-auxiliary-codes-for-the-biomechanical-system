e52e53631c7b919e20ef91964d81d33eb053fe7b
Caio Alves Fiorotti <caiofiorotti@gmail.com>
2024-06-13 10:24:08 -0300

    :sparkles: getting the fisrt two values

--- Scripts python/serial_read.py
+++ Scripts python/serial_read.py
@@ -2,26 +2,26 @@ import serial
 import csv
 
 # Serial Port Settings
-porta_serial = serial.Serial('COM9', baudrate=9600)
-file_name = 'flexion.csv' # flexion.csv or extension.csv
+serial_port = serial.Serial('COM9', baudrate=9600)
+file_name = 'test.csv' # flexion.csv or extension.csv
 
 try:
     with open(file_name, 'w', newline='') as arquivo_csv:
         writer = csv.writer(arquivo_csv)  # Definindo o delimitador como ';'
 
         while True:
-            data = porta_serial.readline().decode('utf-8').strip() # Read data from serial port
+            data = serial_port.readline().decode('utf-8').strip() # Read data from serial port
 
-            print(data)
-            
             # Split the data string into a list of values
-            valores = data.split(',')
-            
+            values = data.split(',')[0:2] # Get the first two values
+
+            print(values)
+
             # Writing to CSV file
-            writer.writerow(valores)
+            writer.writerow(values)
 
 except KeyboardInterrupt:
     print("Stopped by user")
 
 # Close the connection to the serial port
-porta_serial.close()
+serial_port.close()
